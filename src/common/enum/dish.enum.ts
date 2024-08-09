3e9dff22fcc2a7b010cb60f85647a1bf590aec1e
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2024-08-09 13:56:16 +0300

    add dish data

---
+++ src/common/enum/dish.enum.ts
@@ -0,0 +1,9 @@
+export enum DishType {
+  Pasta = 'Паста',
+  Tortellini = 'Тортеліні',
+  ParmesanRisotto = 'Пармезанове різотто',
+  Roquette = 'Роклетто',
+  Salad = 'Салати',
+  Soup = 'Суп',
+  Drink = 'Напій',
+}
