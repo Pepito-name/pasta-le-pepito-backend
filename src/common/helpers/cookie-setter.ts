b1bd8dd450e5aa1c44085a8b6866401eabd1959c
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2024-12-06 21:55:33 +0200

    update

--- src/common/helpers/cookie-setter.ts
+++ src/common/helpers/cookie-setter.ts
@@ -3,8 +3,8 @@ import { Response } from 'express';
 const setRefreshTokenCookie = (response: Response, refreshToken: string) => {
   response.cookie('refresh_token', refreshToken, {
     httpOnly: true,
-    expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
-    sameSite: 'none',
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    sameSite: 'strict',
     secure: true,
   });
 };
